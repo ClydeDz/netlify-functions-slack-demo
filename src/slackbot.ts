b0b1916785463c0c123bc3938e329466027855f7
clydedz <clyd94@gmail.com>
2021-04-17 19:36:30 +1200

    Add environment variables

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,4 +1,6 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
+import * as dotenv from 'dotenv'
+dotenv.config();
 
 export async function handler(event: APIGatewayEvent, context: Context) {
   return {
