b66bf0e4b6bb1474c6fcfcf6c5401c70cc0c9711
clydedz <clyd94@gmail.com>
2021-04-18 00:53:44 +1200

    Listen for a message and respond

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,4 +1,4 @@
-import { App, ExpressReceiver } from '@slack/bolt'
+import { App, ExpressReceiver, ReceiverEvent } from '@slack/bolt'
 import { APIGatewayEvent, Context } from 'aws-lambda'
 import * as dotenv from 'dotenv'
 dotenv.config();
@@ -12,6 +12,10 @@ const app = new App({
   signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
   token: `${process.env.SLACK_BOT_TOKEN}`,
   receiver: expressReceiver
+});
+
+app.message(async ({ say }) => {
+  await say("Hi :wave:");
 });
 
 function parseRequestBody(stringBody: string | null) {
@@ -30,6 +34,21 @@ export async function handler(event: APIGatewayEvent, context: Context) {
       body: payload.challenge
     };
   }
+
+  const slackEvent: ReceiverEvent = {
+    body: payload,
+    ack: async (response) => {
+      return new Promise<void>((resolve, reject) => {
+        resolve();
+        return {
+          statusCode: 200,
+          body: response ?? ""
+        };
+      });
+    },
+  };
+
+  await app.processEvent(slackEvent);
 
   return {
     statusCode: 200,
